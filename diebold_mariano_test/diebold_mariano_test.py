a035e5e280cb35e7c6f113b81bb1b04fe46bf7fe
EchoSun <echosun1996@126.com>
2020-11-11 18:05:35 +0800

    0.0.7 two-tailed test cul P

--- diebold_mariano_test/diebold_mariano_test.py
+++ diebold_mariano_test/diebold_mariano_test.py
@@ -1,7 +1,7 @@
 import math
 
 from scipy.stats import norm
-from scipy.stats import t
+import numpy as np
 
 
 def MSE(T, ori_list, alg_list):
@@ -12,11 +12,6 @@ def MSE(T, ori_list, alg_list):
     :param alg_list: 要比较的的算法
     :return: 返回 g(e{it})
     """
-    # ret = 0
-    # for t in range(T):
-    #     ret += pow((alg_list[t] - ori_list[t]), 2)
-
-    # for t in range(T):
     ret = pow((alg_list[T - 1] - ori_list[T - 1]), 2)
     return ret
 
@@ -31,7 +26,7 @@ def MAE(T, ori_list, alg_list):
     """
     # ret = 0
     # for t in range(T):
-    ret = abs((alg_list[T-1] - ori_list[T-1]))
+    ret = abs((alg_list[T - 1] - ori_list[T - 1]))
     return ret
 
 
@@ -87,10 +82,6 @@ def cul_widehat_gamma_d_tau_list(d_t_list):
         for t in range(tau, len(d_t_list)):
             temp += (d_t_list[t] - overline_d) * (d_t_list[t - tau] - overline_d)
         widehat_gamma_d_tau_list.append(temp / len(d_t_list))
-    # temp = 0
-    # for lag in range(0):
-    #     widehat_gamma_d_tau_list.append(autocovariance(d_t_list, len(d_t_list), lag, overline_d))
-    # widehat_gamma_d_tau_list.append(temp / len(d_t_list))
     return widehat_gamma_d_tau_list
 
 
@@ -104,18 +95,8 @@ def cul_DM(d_t_list):
     T = len(d_t_list)
     widehat_gamma_d_tau_list = cul_widehat_gamma_d_tau_list(d_t_list)
     temp = widehat_gamma_d_tau_list[0]
-    # temp = (widehat_gamma_d_tau_list[0] + 2 * sum(widehat_gamma_d_tau_list[1:])) / T
-    # k = len(widehat_gamma_d_tau_list) - 2  # S(T) = k-1 =T-2
-    # temp = widehat_gamma_d_tau_list[0]  # 2 * pi * widehat_f_d_0
-    # for tau in range(1, k + 1):
-    #     if abs(tau + 1 / k):
-    #         temp += 2 * widehat_gamma_d_tau_list[tau]
     overline_d = cul_overline_d(d_t_list)
-    DM = overline_d /math.sqrt((temp)/T)
-    # DM = 1 / math.sqrt(temp) * overline_d
-    # harvey_adj = ((T + 1 - 2 * tau + tau * (tau - 1) / T) / T) ** (0.5)
-    # DM = DM * harvey_adj
-    # DM = overline_d / math.sqrt(temp / len(widehat_gamma_d_tau_list))
+    DM = overline_d / math.sqrt((temp) / T)
     return DM
 
 
@@ -126,16 +107,14 @@ def cul_P(d_t_list):
     :return: 相伴 P 值
     """
     DM = cul_DM(d_t_list)
-    return 2 * t.cdf(-abs(DM), df=len(d_t_list) - 1)
+    return 2 * norm.cdf(-np.abs(DM))
 
 
 if __name__ == '__main__':
     ori_list = [3, 5, 4, 1]
     a1_list = [2, 3, 4, 2]
     a2_list = [3, 2, 2, 4]
-    d_t_list = cul_d_t(MAE, ori_list, a1_list, a2_list)
-    # # print(d_t_list)
-    # print(cul_widehat_gamma_d_tau_list(d_t_list))
+    d_t_list = cul_d_t(MSE, ori_list, a1_list, a2_list)
     print("Original Data:")
     print(ori_list)
     print("Prediction Algorithm 1:")
@@ -143,5 +122,5 @@ if __name__ == '__main__':
     print("Prediction Algorithm 2:")
     print(a2_list)
     print("#######################")
-    print("DM: "+str(cul_DM(d_t_list)))
-    print("P: "+str(cul_P(d_t_list)))
+    print("DM: " + str(cul_DM(d_t_list)))
+    print("P: " + str(cul_P(d_t_list)))
